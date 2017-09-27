1a48eb76f84584866ab730aac0e57cf797d70dd0
Enrico Bernardo <ebernardo@gmail.com>
2017-09-26 19:16:34 -0700

    adding URI to request

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -30,7 +30,7 @@ function getShopDotComProduct(productWanted, parentElement, clickCb) {
 
     for (var i = 0; i < 12; i++) {
       // Creating a div to hold the giphys
-      var giphyDiv = $("<div class='giphy col-xs-12 col-sm-6 col-md-4 col-lg-4'>");
+      var giphyDiv = $("<div class='giphy col-xs-4 col-sm-4 col-md-3 col-lg-3'>");
 
       // Storing the product name
       var ItemName = products[i].caption;
