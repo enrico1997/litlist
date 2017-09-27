eff1b43ee12231ed4d6a01f87108c9ab94055433
zazil <zaziltoma@gmail.com>
2017-09-26 22:46:28 -0700

    add local storage, header name, css changes, cleanup

--- assets/javascript/firebaseCalls.js
+++ assets/javascript/firebaseCalls.js
@@ -50,45 +50,46 @@ database.ref("/users").on("child_added", function(childSnapshot) {
         $("<img>").addClass("img img-thumbnail img-responsive center-block").attr("src", userImage + ".jpg").attr('id', userGuid).attr('val',userName))
       );
 
-     
 
 }); // end of fb listener
 
   $(document).on('click', 'img', function(){
-    alert("clicked")
-    //replace id with this.id
 
-    //console.log(database.ref("/users/" + this.id).once("value"));
     database.ref("/users/" + this.id).once("value").then(function(snap){
 
+      var users = snap.val();
+      var items = users.items;
+      for(var key in items){
+        console.log("category ", items[key].nitemCategory);
+        console.log("Id ", items[key].nitemId);
+        console.log("name ", items[key].nitemName);
+        console.log("review ", items[key].nitemReview);
 
-        console.log(snap);
-        var users = snap.val();
-        var items = users.items
-        for(var key in items){
-          console.log("category ", items[key].nitemCategory);
-          console.log("Id ", items[key].nitemId);
-          console.log("name ", items[key].nitemName);
-          console.log("review ", items[key].nitemReview);
+        var itemCategory = items[key].nitemCategory;
+        var itemId = items[key].nitemId;
+        var itemName = items[key].nitemName;
+        var itemReview = items[key].nitemReview;
 
-          var itemCategory = items[key].nitemCategory;
-          var itemId = items[key].nitemId;
-          var itemName = items[key].nitemName;
-          var itemReview = items[key].nitemReview;
-
-         $("#productRows")
-    //.append("<tr>").data("id", .key)
+        $("#productRows")
         .append($("<tr>"))
         .append($("<td>" + itemName + "</td>"))
         .append($("<td>" + itemId + "</td>"))
         .append($("<td>" + itemReview + "</td>"))
         .append($("<td>" + itemCategory + "</td>"))
         .append($("</tr>"));
-    }
 
-    
-    //$("#headerName").text(userName + "'s LiTLiST");
+        $("#headerName").text(users.username + "'s LiTLiST");
+
+      } // end of for loop
+
+        // clear local storage
+        localStorage.clear();
+
+        // store Guid into local storage
+        localStorage.setItem("userId", snap.key);
     });
+
     $('#productRows').empty();
+
   });
 
